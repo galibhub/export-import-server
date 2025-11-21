33746c505196d835319f425e31020113a59a7c7d
Ibrahim Ahmed Galib <galib@Ibrahims-Mac-mini.local>
2025-11-21 16:47:56 +0600

    deploy problem solve

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 //middleware
 app.use(cors({
-  origin:["http://localhost:5173",]
+  origin:["http://localhost:5173","https://export-import-hub.netlify.app"]
 }));
 app.use(express.json());
 
